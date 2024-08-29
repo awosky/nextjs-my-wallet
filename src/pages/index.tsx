85bd7bfd0b84899c9ceb74335d1102982b5fbad0
Awosky <playergames998@gmail.com>
2024-08-29 20:14:06 +0700

    Removed redundant code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,6 @@ export default function Home() {
           name="description"
           content="Our wallet website helps you manage your money online. With easy-to-use tools, you can track your expenses, income, and make secure transactions. Our platform is designed to simplify financial management for everyone, whether you're a student or a business owner."
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
