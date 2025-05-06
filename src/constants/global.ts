5312809a849fd846196d78f63388483d12539ac4
Awosky <playergames998@gmail.com>
2025-05-06 20:59:36 +0700

    Update categories

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -9,16 +9,16 @@ export const CATEGOTY_PROPERTIES: CategoryProperties = {
   Zakat: { icon: "ApartmentOutlined", color: "#62DAAB" },
   Family: { icon: "TeamOutlined", color: "#6395F9" },
   "Boarding House": { icon: "HomeOutlined", color: "#657798" },
-  Sedekah: { icon: "GiftOutlined", color: "#F6C022" },
+  Charity: { icon: "GiftOutlined", color: "#F6C022" },
   Investment: { icon: "BarChartOutlined", color: "#7666F9" },
   Electricity: { icon: "ThunderboltOutlined", color: "#0081C9" },
-  "Internet Data": { icon: "WifiOutlined", color: "#815B5B" },
-  ATM: { icon: "CreditCardOutlined", color: "#41644A" },
+  Internet: { icon: "WifiOutlined", color: "#815B5B" },
+  "Cash Withdrawal": { icon: "CreditCardOutlined", color: "#41644A" },
   Shopping: { icon: "ShoppingCartOutlined", color: "#EB455F" },
   Life: { icon: "RocketOutlined", color: "#E86A33" },
-  "Laundry Service": { icon: "ContainerOutlined", color: "#72FFFF" },
+  Laundry: { icon: "ContainerOutlined", color: "#72FFFF" },
   "E-Wallet": { icon: "WalletOutlined", color: "#2F8F9D" },
-  Subcription: { icon: "LaptopOutlined", color: "#FFE7CC" },
+  Subscription: { icon: "LaptopOutlined", color: "#FFE7CC" },
   Others: { icon: "GlobalOutlined", color: "#2C3333" },
 };
 
@@ -26,6 +26,14 @@ export const INCOME_CATEGORY_OPTIONS = [
   {
     value: "Salary",
     label: "Salary",
+  },
+  {
+    value: "Bonus",
+    label: "Bonus",
+  },
+  {
+    value: "Others",
+    label: "Others",
   },
 ];
 
@@ -43,8 +51,8 @@ export const EXPENSE_CATEGORY_OPTIONS = [
     label: "Boarding House",
   },
   {
-    value: "Sedekah",
-    label: "Sedekah",
+    value: "Charity",
+    label: "Charity",
   },
   {
     value: "Investment",
@@ -55,12 +63,12 @@ export const EXPENSE_CATEGORY_OPTIONS = [
     label: "Electricity",
   },
   {
-    value: "Internet Data",
-    label: "Internet Data",
+    value: "Internet",
+    label: "Internet",
   },
   {
-    value: "ATM",
-    label: "ATM",
+    value: "Cash Withdrawal",
+    label: "Cash Withdrawal",
   },
   {
     value: "Shopping",
@@ -71,16 +79,16 @@ export const EXPENSE_CATEGORY_OPTIONS = [
     label: "Life",
   },
   {
-    value: "Laundry Service",
-    label: "Laundry Service",
+    value: "Laundry",
+    label: "Laundry",
   },
   {
     value: "E-Wallet",
     label: "E-Wallet",
   },
   {
-    value: "Subcription",
-    label: "Subcription",
+    value: "Subscription",
+    label: "Subscription",
   },
   {
     value: "Others",
