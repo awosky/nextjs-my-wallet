2076ce3a518819dcd803aebd749579844b1da232
Awosky <playergames998@gmail.com>
2026-02-01 20:21:55 +0700

    make navbar become sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,28 @@
 import { PlusCircleTwoTone } from "@ant-design/icons";
 import { Button, Col, Row, Typography } from "antd";
+import clsx from "clsx";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ModalForm from "./ModalForm";
 import style from "./Navbar.module.scss";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <>
-      <Row className={style.navbar} justify="space-between" align="middle" wrap={false}>
+      <Row className={clsx(style.navbar, { [style.scrolled]: scrolled })} justify="space-between" align="middle" wrap={false}>
         <Col span={4}>
           <Image width={32} height={32} alt="logo" src="/images/logo.png" className={style.logo} />
         </Col>
